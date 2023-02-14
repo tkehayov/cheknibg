feb30c127afe257b72351ed3802a990ad53efa02
Tihomir Kehayov <kehayov89@gmail.com>
2023-02-14 18:09:07 +0200

    remove images column from products table

--- products/src/test/resources/changelog/changes/7.insert-products.sql
+++ products/src/test/resources/changelog/changes/7.insert-products.sql
@@ -1,7 +1,7 @@
-INSERT INTO products ("name",images,category,code_id) VALUES
-	 ('Apple iPhone 14 Pro 128GB Мобилни телефони (GSM)',1,1,'PLT-00008'),
-	 ('Xiaomi Redmi Note 11 128GB 6GB RAM Dual Мобилни телефони (GSM)',2,1,'PLT-12344');
+INSERT INTO products ("name",category,code_id) VALUES
+	 ('Apple iPhone 14 Pro 128GB Мобилни телефони (GSM)',1,'PLT-00008'),
+	 ('Xiaomi Redmi Note 11 128GB 6GB RAM Dual Мобилни телефони (GSM)',1,'PLT-12344');
 
 INSERT INTO images(product_id,filename) values
-((select id from products where images=1),'mac.jpg'),
-((select id from products where images=2),'laptop.jpg');
+((select id from products where code_id='PLT-00008'),'mac.jpg'),
+((select id from products where code_id='PLT-12344'),'laptop.jpg');
