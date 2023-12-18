1e748418e49cd1fdda17c672e9130fb181b2b1b5
Tihomir Kehayov <kehayov89@gmail.com>
2023-12-18 22:12:06 +0200

    13-separate-images-repos (#14)

--- products/src/main/java/com/products/rest/images/ImageController.java
+++ products/src/main/java/com/products/rest/images/ImageController.java
@@ -12,22 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 public class ImageController {
 
     @GetMapping
-    public void method(HttpServletResponse httpServletResponse, HttpServletRequest request) {
+    public void redirect(HttpServletResponse httpServletResponse, HttpServletRequest request) {
 
         String requestURI = request.getRequestURI();
 
         if (requestURI.contains("//cdn.cs")) {
             String[] split = requestURI.split("/https");
-            String image = "https"+split[split.length - 1];
-            System.out.println(image);
+            String image = "https" + split[split.length - 1];
             httpServletResponse.setHeader("Location", image);
         } else {
             String[] split = requestURI.split("/");
-            String image = split[split.length - 1];
-            httpServletResponse.setHeader("Location", "http://localhost:8081/" + image);
+            String image = split[split.length - 2] + "/" + split[split.length - 1];
+            String redirectUrl = "http://localhost:8081/" + image;
+            httpServletResponse.setHeader("Location", redirectUrl);
 
         }
-
         httpServletResponse.setStatus(302);
     }
 }
