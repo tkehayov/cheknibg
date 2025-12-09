08c0340049658493433fc6cf87dff3e862b315c0
Tihomir Kehayov <kehayov89@gmail.com>
2025-12-09 20:34:26 +0200

    121-update-all-products-with-no-categories

---
+++ docker/liquibase/products/changelog/changes/76.update-products-without-categories.sql
@@ -0,0 +1,66 @@
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id='SM-L705FZA2EUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id='SM-Q501NZKAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L705FAW2EUE';
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L705FZB2EUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L705FZS2EUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L505FZKAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L505FZWAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L500NZKAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L500NZWAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L335FDAAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L335FZSAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L330NDAAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L330NZSAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L325FDAAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L325FZSAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L320NDAAEUE';
+
+update products
+set category = (select id from categories where alias = 'watches')
+where code_id ='SM-L320NZSAEUE';
